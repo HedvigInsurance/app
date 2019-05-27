ba74e7d1d8c756299a1df0098439b8a7832bd2ac
Oscar Nylander <oscar@hedvig.com>
2019-05-27 11:43:58 +0200

    wip

---
+++ android/app/src/main/java/com/hedvig/app/feature/whatsnew/PagerIndicatorView.kt
@@ -0,0 +1,13 @@
+package com.hedvig.app.feature.whatsnew
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+
+class PagerIndicatorView: LinearLayout {
+    constructor(context: Context): super(context)
+    constructor(context: Context, attributeSet: AttributeSet?): super(context, attributeSet)
+    constructor(context: Context, attributeSet: AttributeSet?, defStyle: Int): super(context, attributeSet, defStyle)
+
+
+}
