e59cb68daa22e4f2dbe6fd15f9af3a33e1a13e61
Fredrik Areschoug <fredrik.areschoug@gmail.com>
2019-06-14 15:55:41 +0200

    removed redundent view

--- android/app/src/main/java/com/hedvig/app/feature/referrals/ReferralsReceiverActivity.kt
+++ android/app/src/main/java/com/hedvig/app/feature/referrals/ReferralsReceiverActivity.kt
@@ -3,18 +3,12 @@ package com.hedvig.app.feature.referrals
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
-import com.hedvig.android.owldroid.graphql.ReferralCampaignMemberInformationQuery
 import com.hedvig.android.owldroid.type.RedeemCodeStatus
 import com.hedvig.app.BaseActivity
 import com.hedvig.app.R
 import com.hedvig.app.feature.chat.ChatActivity
-import com.hedvig.app.util.extensions.getReferralsCode
 import com.hedvig.app.util.extensions.observe
-import com.hedvig.app.util.extensions.removeReferralsCode
-import com.hedvig.app.util.extensions.view.remove
-import com.hedvig.app.util.extensions.view.show
 import com.hedvig.app.util.interpolateTextKey
-import kotlinx.android.synthetic.main.chat_activity.*
 import kotlinx.android.synthetic.main.referrals_receiver_activity.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
@@ -32,7 +26,7 @@ class ReferralsReceiverActivity : BaseActivity() {
                     RedeemCodeStatus.ACCEPTED -> startChat()
                     else -> {
                         //todo handle can't redeem code
-                        Toast.makeText(this@ReferralsReceiverActivity, "The code ${getReferralsCode()} is invalid!", Toast.LENGTH_LONG).show()
+                        Toast.makeText(this@ReferralsReceiverActivity, "The code ${intent.getStringExtra(EXTRA_REFERRAL_CODE)} is invalid!", Toast.LENGTH_LONG).show()
                     }
                 }
             }
@@ -41,14 +35,11 @@ class ReferralsReceiverActivity : BaseActivity() {
             referralViewModel.redeemReferralCode(intent.getStringExtra(EXTRA_REFERRAL_CODE))
         }
         referralReceiverContinueWithoutButton.setOnClickListener {
-            removeReferralsCode()
             startChat()
         }
         referralsReceiverTitle.text = interpolateTextKey(
             getString(R.string.REFERRAL_STARTSCREEN_HEADLINE),
             "REFERRAL_VALUE" to intent.getStringExtra(EXTRA_REFERRAL_INCENTIVE))
-        referralsReceiverInformationContainer.show()
-        loadingSpinner.remove()
     }
 
     private fun startChat() {
