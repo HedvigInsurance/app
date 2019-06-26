dc2f65f114b479c1914575420897e5efe5a7f4ee
Fredrik Areschoug <fredrik.areschoug@gmail.com>
2019-06-26 20:31:08 +0200

    remove notification dot in tab bar

--- android/app/src/main/java/com/hedvig/app/feature/referrals/ReferralsFragment.kt
+++ android/app/src/main/java/com/hedvig/app/feature/referrals/ReferralsFragment.kt
@@ -7,6 +7,7 @@ import com.hedvig.android.owldroid.graphql.ProfileQuery
 import com.hedvig.app.BuildConfig
 import com.hedvig.app.R
 import com.hedvig.app.feature.loggedin.ui.BaseTabFragment
+import com.hedvig.app.feature.loggedin.ui.BaseTabViewModel
 import com.hedvig.app.feature.profile.service.ProfileTracker
 import com.hedvig.app.feature.profile.ui.ProfileViewModel
 import com.hedvig.app.feature.profile.ui.referral.InvitesAdapter
@@ -24,6 +25,8 @@ import timber.log.Timber
 class ReferralsFragment : BaseTabFragment() {
     private val tracker: ProfileTracker by inject()
     private val profileViewModel: ProfileViewModel by sharedViewModel()
+
+    private val tabViewModel: BaseTabViewModel by sharedViewModel()
 
     override val layout = R.layout.fragment_new_referral
 
@@ -56,6 +59,7 @@ class ReferralsFragment : BaseTabFragment() {
                 }
             }
         }
+        tabViewModel.removeReferralNotification()
     }
 
     private fun bindData(monthlyCost: Int, data: ProfileQuery.ReferralInformation) {
