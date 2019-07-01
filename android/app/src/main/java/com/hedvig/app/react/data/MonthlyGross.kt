e41eb6030ab415003d727664b21d7c4c677b409f
Fredrik Areschoug <fredrik.areschoug@gmail.com>
2019-07-01 15:15:12 +0200

    mapped cost

---
+++ android/app/src/main/java/com/hedvig/app/react/data/MonthlyGross.kt
@@ -0,0 +1,6 @@
+package com.hedvig.app.react.data
+
+data class MonthlyGross(
+    val __typename: String,
+    val amount: String
+)
---
+++ android/app/src/main/java/com/hedvig/app/react/data/MonthlyNet.kt
@@ -0,0 +1,6 @@
+package com.hedvig.app.react.data
+
+data class MonthlyNet(
+    val __typename: String,
+    val amount: String
+)
