f33de66979c852044b9beacadd5b942b76aed898
Fredrik Areschoug <fredrik.areschoug@gmail.com>
2019-07-03 12:25:10 +0200

    Text input feild

--- android/app/src/main/java/com/hedvig/app/feature/chat/native/ChatInputView.kt
+++ android/app/src/main/java/com/hedvig/app/feature/chat/native/ChatInputView.kt
@@ -1,9 +1,14 @@
 package com.hedvig.app.feature.chat.native
 
 import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.AttributeSet
+import android.widget.EditText
 import android.widget.FrameLayout
 import com.hedvig.app.R
+import com.hedvig.app.util.extensions.makeToast
+import kotlinx.android.synthetic.main.chat_input_view.view.*
 
 class ChatInputView : FrameLayout {
     constructor(context: Context) : super(context)
@@ -12,5 +17,9 @@ class ChatInputView : FrameLayout {
 
     init {
         inflate(context, R.layout.chat_input_view, this)
+        inputText.sendClickListener = {
+            context.makeToast("Send text ${inputText.currentMessage}")
+        }
     }
+
 }
