ab090b3512714bb33ff6d1fb9684b1b935ce0594
Fredrik Areschoug <fredrik.areschoug@gmail.com>
2019-07-04 11:54:28 +0200

    handle dynamic link

--- android/app/src/main/java/com/hedvig/app/SplashActivity.kt
+++ android/app/src/main/java/com/hedvig/app/SplashActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.dynamiclinks.FirebaseDynamicLinks
 import com.hedvig.app.feature.marketing.ui.MarketingActivity
@@ -13,7 +14,6 @@ import com.hedvig.app.feature.referrals.ReferralsReceiverActivity
 import com.hedvig.app.service.LoginStatus
 import com.hedvig.app.service.LoginStatusService
 import com.hedvig.app.util.extensions.compatColor
-import com.hedvig.app.util.safeLet
 import com.hedvig.app.util.whenApiVersion
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.rxkotlin.plusAssign
@@ -25,19 +25,19 @@ class SplashActivity : BaseActivity() {
 
     private val loggedInService: LoginStatusService by inject()
 
-    private var referralCode: String? = null
-    private var referralIncentive: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         whenApiVersion(Build.VERSION_CODES.M) {
             window.statusBarColor = compatColor(R.color.off_white)
         }
+
+        Log.e("SplashActivity", "onCreate")
     }
 
     override fun onStart() {
         super.onStart()
 
+        Log.e("SplashActivity", "onStart")
         handleFirebaseDynamicLink(intent)
 
         disposables += loggedInService
@@ -49,15 +49,19 @@ class SplashActivity : BaseActivity() {
 
     override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
+        Log.e("SplashActivity", "onNewIntent")
         handleFirebaseDynamicLink(intent)
     }
 
     private fun handleFirebaseDynamicLink(intent: Intent) {
+        Log.e("SplashActivity", "data: ${intent.data}")
         FirebaseDynamicLinks.getInstance().getDynamicLink(intent).addOnSuccessListener { pendingDynamicLinkData ->
+            Log.e("SplashActivity", "SuccessListener")
             if (pendingDynamicLinkData != null && pendingDynamicLinkData.link != null) {
                 val link = pendingDynamicLinkData.link
                 val referee = link.getQueryParameter("memberId")
                 val incentive = link.getQueryParameter("incentive")
+                Log.e("SplashActivity", "link: $link")
                 if (referee != null && incentive != null) {
                     getSharedPreferences("referrals", Context.MODE_PRIVATE).edit().putString("referee", referee)
                         .putString("incentive", incentive).apply()
@@ -69,25 +73,21 @@ class SplashActivity : BaseActivity() {
                     FirebaseAnalytics.getInstance(this).logEvent("referrals_open", b)
                 }
 
-                handleNewReferralLink(link)
+                link.getQueryParameter("code")?.let { referralCode ->
+                    startActivity(ReferralsReceiverActivity.newInstance(this, referralCode, "10")) //Fixme "10" should not be hard coded
+                }
+            } else {
+                startActivity(Intent(this, MarketingActivity::class.java))
             }
+        }.addOnFailureListener {
+            startActivity(Intent(this, MarketingActivity::class.java))
         }
-    }
-
-    private fun handleNewReferralLink(link: Uri) {
-        referralCode = link.getQueryParameter("code")
-        referralIncentive = "10" //FIXME: this must be changed when we hav an other incentive then 10
     }
 
     private fun navigateToActivity(loginStatus: LoginStatus) = when (loginStatus) {
-        LoginStatus.ONBOARDING -> {
-            safeLet(referralCode, referralIncentive) { referralCode, incentive ->
-                startActivity(ReferralsReceiverActivity.newInstance(this, referralCode, incentive))
-            } ?: startActivity(Intent(this, MarketingActivity::class.java))
-        }
+        LoginStatus.ONBOARDING -> handleFirebaseDynamicLink(intent)
         LoginStatus.IN_OFFER -> {
             val intent = Intent(this, OfferActivity::class.java)
-            referralCode?.let { intent.putExtra(OfferActivity.EXTRA_REFERRAL_CODE, it) }
             startActivity(intent)
         }
         LoginStatus.LOGGED_IN -> startActivity(Intent(this, LoggedInActivity::class.java))
