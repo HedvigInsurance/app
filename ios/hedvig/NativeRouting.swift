464ac1cd34dfe67bc429edf9d35effc58e35b768
Gustaf Gunér <gustaf@guner.se>
2019-07-09 13:46:42 +0200

    Fix peril draggable overlay title on offer screen

--- ios/hedvig/NativeRouting.swift
+++ ios/hedvig/NativeRouting.swift
@@ -102,7 +102,7 @@ class NativeRouting: RCTEventEmitter {
             }
 
             let perilInformation = PerilInformation(
-                title: title,
+                title: title.replacingOccurrences(of: “-\n”, with: “”)
                 description: description,
                 icon: Peril.iconAsset(for: idString)
             )
