9ec2776c1d0f7477cef8a91cef5c6028bf7201b5
Oscar Nylander <oscar@hedvig.com>
2019-07-24 14:28:53 +0200

    Bubbles
    No animation for now

--- android/app/src/main/java/com/hedvig/app/feature/offer/OfferActivity.kt
+++ android/app/src/main/java/com/hedvig/app/feature/offer/OfferActivity.kt
@@ -11,16 +11,22 @@ import com.hedvig.app.R
 import com.hedvig.app.feature.dashboard.ui.PerilBottomSheet
 import com.hedvig.app.feature.dashboard.ui.PerilIcon
 import com.hedvig.app.feature.dashboard.ui.PerilView
+import com.hedvig.app.util.extensions.compatColor
 import com.hedvig.app.util.extensions.displayMetrics
 import com.hedvig.app.util.extensions.observe
+import com.hedvig.app.util.extensions.setStrikethrough
+import com.hedvig.app.util.extensions.view.hide
 import com.hedvig.app.util.extensions.view.remove
 import com.hedvig.app.util.extensions.view.setHapticClickListener
 import com.hedvig.app.util.extensions.view.show
 import com.hedvig.app.util.interpolateTextKey
+import com.hedvig.app.util.isApartmentOwner
 import com.hedvig.app.util.isStudentInsurance
 import com.hedvig.app.util.safeLet
 import kotlinx.android.synthetic.main.activity_offer.*
+import kotlinx.android.synthetic.main.feature_bubbles.*
 import kotlinx.android.synthetic.main.loading_spinner.*
+import kotlinx.android.synthetic.main.price_bubbles.*
 import kotlinx.android.synthetic.main.offer_peril_section.view.*
 import kotlinx.android.synthetic.main.offer_section_terms.view.*
 import org.koin.android.viewmodel.ext.android.viewModel
@@ -54,14 +60,65 @@ class OfferActivity : AppCompatActivity() {
             startActivity(Intent(Intent.ACTION_VIEW, PRIVACY_POLICY_URL))
         }
 
+        grossPremium.setStrikethrough(true)
+
         offerViewModel.data.observe(lifecycleOwner = this) { data ->
             data?.let { d ->
                 loadingSpinner.remove()
                 container.show()
+                bindPriceBubbles(d)
+                bindFeatureBubbles(d)
                 bindHomeSection(d)
                 bindStuffSection(d)
                 bindMeSection(d)
                 bindTerms(d)
+            }
+        }
+    }
+
+    private fun bindPriceBubbles(data: OfferQuery.Data) {
+        grossPremium.hide()
+        discountBubble.hide()
+        discountTitle.hide()
+
+        netPremium.setTextColor(compatColor(R.color.off_black_dark))
+        netPremium.text = data.insurance.cost?.monthlyNet?.amount?.toBigDecimal()?.toInt()?.toString()
+        if (data.redeemedCampaigns.size > 0) {
+            when (data.redeemedCampaigns[0].incentive) {
+                is OfferQuery.AsMonthlyCostDeduction -> {
+                    grossPremium.show()
+                    grossPremium.text = data.insurance.cost?.monthlyGross?.amount?.toBigDecimal()?.toInt()?.toString()
+                    discountBubble.show()
+                    discount.text = getString(R.string.OFFER_SCREEN_INVITED_BUBBLE)
+                    netPremium.setTextColor(compatColor(R.color.pink))
+                }
+                is OfferQuery.AsFreeMonths -> {
+                    discountTitle.show()
+                    discount.text = interpolateTextKey(
+                        getString(R.string.OFFER_SCREEN_FREE_MONTHS_BUBBLE),
+                        "free_month" to (data.redeemedCampaigns[0].incentive as OfferQuery.AsFreeMonths).quantity
+                    )
+                }
+            }
+        }
+    }
+
+    private fun bindFeatureBubbles(data: OfferQuery.Data) {
+        amountInsured.text = interpolateTextKey(
+            getString(R.string.OFFER_BUBBLES_INSURED_SUBTITLE),
+            "personsInHousehold" to data.insurance.personsInHousehold
+        )
+        startDate.text = if (data.insurance.insuredAtOtherCompany == true) {
+            getString(R.string.OFFER_BUBBLES_START_DATE_SUBTITLE_SWITCHER)
+        } else {
+            getString(R.string.OFFER_BUBBLES_START_DATE_SUBTITLE_NEW)
+        }
+
+        data.insurance.type?.let { t ->
+            brfOrTravel.text = if (isApartmentOwner(t)) {
+                getString(R.string.OFFER_BUBBLES_OWNED_ADDON_TITLE)
+            } else {
+                getString(R.string.OFFER_BUBBLES_TRAVEL_PROTECTION_TITLE)
             }
         }
     }
